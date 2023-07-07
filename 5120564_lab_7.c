5c787eae4598ac45bbc2b478f546a4ee60e5ec64
BrianEricson1 <115884718+BrianEricson1@users.noreply.github.com>
2023-07-07 00:15:16 -0400

    Add files via upload

---
+++ 5120564_lab_7.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+void swap(int arr[], int a, int b){
+  int temp = arr[b];
+  arr[b] = arr[a];
+  arr[a] = temp;
+}
+
+int numOfSwaps(int arr[], int size){
+  int count = 0; //counts # of swaps
+  for(int i = 0; i < size; i++){
+    for(int j = 0; j < size-(i+1); j++){
+      // size-(i+1) indicates the index at which the following elements in the list has been sorted
+      //from size-1 (no elements sorted) to -1 (all elements sorted)
+      if(arr[j] >= arr[j+1]){
+        swap(arr, j, j+1);
+        count ++;
+      }
+    }
+  }
+  return count;
+}
+
+void main(void) {
+  int arr[] = {97,16,45,63,13,22,7,58,72};
+  /* #0:     97,16,45,63,13,22,7,58,72
+     #1: 8 - 16,45,63,13,22,7,58,72,97
+     #2: 4 - 16,45,13,22,7,58,63,72,97
+     #3: 3 - 16,13,22,7,45,58,63,72,97
+     #4: 2 - 13,16,7,22,45,58,63,72,97
+     #5: 1 - 13,7,16,22,45,58,63,72,97
+     #6: 1 - 7,13,16,22,45,58,63,72,97
+  */
+  printf("\nList before bubble sort: ");
+  for(int i = 0; i < 9; i++)
+    printf("%d ", arr[i]);
+  printf("\n\nNumber of Swaps: %d", numOfSwaps(arr,9));
+  printf("\n\nList after bubble sort: ");
+  for(int i = 0; i < 9; i++)
+    printf("%d ", arr[i]);
+}
